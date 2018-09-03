1808a22ae1cb86afcc966b6002a2965ed68ad246
MatthewDodi <matthew.dodi@gmail.com>
2018-09-03 13:04:20 -0700

    changed logo's height

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,14 +3,14 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import styled from 'styled-components';
 
 const LogoImage = styled.img`
-    height: ${props => props.height };
+    height: 50px;
     filter: grayscale(100%);
     margin: 0 auto;
     display: block;
 `
 
 const logo = (props) => (
-    <LogoImage height={props.height} src={burgerLogo} alt="Burgatory Logo"/>
+    <LogoImage src={burgerLogo} alt="Burgatory Logo"/>
 );
 
 export default logo;
