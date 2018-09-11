73cd8ee04e06fb6b753255214bd2823bbf6d0ae4
MatthewDodi <matthew.dodi@gmail.com>
2018-09-11 00:46:04 -0700

    added styling to the orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import BurgerIngredients from '../Burger/BurgerIngredients/BurgerIngredients';
 
 const OrderContainer = styled.div` 
     width: 80%;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 3px rgba(0,0,0,.25);
+    border: 1px solid rgba(0,0,0,.2);
+    box-shadow: 0 4px 5px rgba(0,0,0,.25);
+    border-radius: 15px;
     padding: 10px;
     margin: 10px auto;
     box-sizing: border-box;
@@ -13,19 +13,24 @@ const OrderContainer = styled.div`
 
 const List = styled.ul`
     list-style: none;
-    padding: 30px;
+    padding: 10px 22px;
+    margin: 0;
 `
 
 const ListItem = styled.li`
     display: inline-block;
-    margin-right: 1rem;
+    margin: 0 3rem 2rem 0;
+    padding: 10px;
+    &::first-letter {
+        text-transform: capitalize;
+    }
 `
 
 const order = (props) => (
     <OrderContainer>
         <List>
             {Object.keys(props.ingredients).map(ing => (
-                    <ListItem>{ing} : {props.ingredients[ing]}</ListItem>
+                    <ListItem key={ing} >{ing} : {props.ingredients[ing]}</ListItem>
             ))}
         </List>
         <p style={{marginLeft: '2rem'}}>Price <strong>$ {Number.parseFloat(props.price).toFixed(2)}</strong></p>
